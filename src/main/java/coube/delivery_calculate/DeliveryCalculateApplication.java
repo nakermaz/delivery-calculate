2b13627d0756a1fa79f484bf57fc2c7063207a78
nakermaz <telvintamer@gmail.com>
2026-04-01 20:30:07 +0500

    начальный каркас

---
+++ src/main/java/coube/delivery_calculate/DeliveryCalculateApplication.java
@@ -0,0 +1,13 @@
+package coube.delivery_calculate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryCalculateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryCalculateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/coube/delivery_calculate/DeliveryCalculateApplicationTests.java
@@ -0,0 +1,13 @@
+package coube.delivery_calculate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryCalculateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
